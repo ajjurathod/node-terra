189cadcf593b8989e4e1fd2ae092663db3b6fc6d
ajjurathod <131947107+ajjurathod@users.noreply.github.com>
2025-06-06 12:42:42 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React UI!</h1>
+      <p>This is the frontend app deployed via the DevSecOps pipeline.</p>
+    </div>
+  );
+}
+
+export default App;
